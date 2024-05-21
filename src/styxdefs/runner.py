8a7764517425136ae7d223aa987e5fb56c00cdc4
Florian Rupprecht <floruppr@gmail.com>
2024-05-21 18:30:16 -0400

    Default runner implementation

--- src/styxdefs/runner.py
+++ src/styxdefs/runner.py
@@ -1,6 +1,8 @@
 """Default runner implementation."""
 
 import logging
+import os
+import pathlib
 from collections import deque
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
@@ -9,15 +11,61 @@ from subprocess import PIPE, CalledProcessError, Popen
 from .types import Execution, InputPathType, Metadata, OutputPathType, Runner
 
 
-class DefaultRunner(Runner, Execution):
+class _DefaultExecution(Execution):
+    """Default execution implementation."""
+
+    def __init__(self, logger: logging.Logger, dir: pathlib.Path) -> None:
+        """Initialize the execution."""
+        self.logger: logging.Logger = logger
+        self.dir: pathlib.Path = dir
+
+    def input_file(self, host_file: InputPathType) -> str:
+        """Resolve host input files."""
+        return str(host_file)
+
+    def output_file(self, local_file: str, _optional: bool = False) -> OutputPathType:
+        """Resolve local output files."""
+        return str(self.dir / local_file)
+
+    def run(self, cargs: list[str]) -> None:
+        """Run the command."""
+        self.last_cargs = cargs
+
+        def _stdout_handler(line: str) -> None:
+            self.logger.info(line)
+
+        def _stderr_handler(line: str) -> None:
+            self.logger.error(line)
+
+        while self.dir.exists():
+            self.logger.warning(
+                f"Execution directory {self.dir} already exists. Trying another."
+            )
+            self.dir = self.dir.with_name(f"{self.dir.name}_1")
+        self.dir.mkdir(parents=True, exist_ok=True)
+
+        with Popen(cargs, text=True, stdout=PIPE, stderr=PIPE, cwd=self.dir) as process:
+            with ThreadPoolExecutor(2) as pool:  # two threads to handle the streams
+                exhaust = partial(pool.submit, partial(deque, maxlen=0))
+                exhaust(_stdout_handler(line[:-1]) for line in process.stdout)  # type: ignore
+                exhaust(_stderr_handler(line[:-1]) for line in process.stderr)  # type: ignore
+        return_code = process.poll()
+        if return_code:
+            raise CalledProcessError(return_code, process.args)
+
+
+class DefaultRunner(Runner):
     """Default runner implementation."""
 
     logger_name = "styx_default_runner"
 
-    def __init__(self) -> None:
+    def __init__(self, data_dir: InputPathType | None = None) -> None:
         """Initialize the runner."""
         self.last_cargs: list[str] | None = None
         self.last_metadata: Metadata | None = None
+        self.data_dir = data_dir or pathlib.Path("styx_tmp")
+        self.uid = os.urandom(8).hex()
+        self.execution_counter = 0
 
         # Configure logger
         self.logger = logging.getLogger(self.logger_name)
@@ -34,34 +82,9 @@ class DefaultRunner(Runner, Execution):
     def start_execution(self, metadata: Metadata) -> Execution:
         """Start a new execution."""
         self.last_metadata = metadata
-        return self
-
-    def input_file(self, host_file: InputPathType) -> str:
-        """Resolve host input files."""
-        return str(host_file)
-
-    def output_file(self, local_file: str, optional: bool = False) -> OutputPathType:
-        """Resolve local output files."""
-        return local_file
-
-    def run(self, cargs: list[str]) -> None:
-        """Run the command."""
-        self.last_cargs = cargs
-
-        def stdout_handler(line: str) -> None:
-            self.logger.info(line)
-
-        def stderr_handler(line: str) -> None:
-            self.logger.error(line)
-
-        with Popen(cargs, text=True, stdout=PIPE, stderr=PIPE) as process:
-            with ThreadPoolExecutor(2) as pool:  # two threads to handle the streams
-                exhaust = partial(pool.submit, partial(deque, maxlen=0))
-                exhaust(stdout_handler(line[:-1]) for line in process.stdout)  # type: ignore
-                exhaust(stderr_handler(line[:-1]) for line in process.stderr)  # type: ignore
-        return_code = process.poll()
-        if return_code:
-            raise CalledProcessError(return_code, process.args)
+        return _DefaultExecution(
+            self.data_dir / f"{self.uid}_{self.execution_counter}_{metadata.name}"
+        )
 
 
 _DEFAULT_RUNNER: DefaultRunner | None = None
